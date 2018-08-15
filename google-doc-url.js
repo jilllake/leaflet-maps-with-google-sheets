83995ad1d069944e33097c15ad40a2c655e81433
Jill <jill.lake@ars.usda.gov>
2018-08-15 07:29:37 -0400

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,1 +1,1 @@
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/1ZxvU8eGyuN9M8GxTU9acKVJv70iC3px_m3EVFsOHN9g/edit#gid=0';
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/1YUt2n2bqTHm9MBrkTpoOHQp5JLfsxRWXAK4YULbOFLA/edit#gid=0';
